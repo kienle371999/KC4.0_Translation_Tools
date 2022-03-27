d2775fa43317a6b1145175b3badb2b0795009c68
thanhphuonghdhh <19021346@vnu.edu.vn>
2022-03-27 23:05:34 +0700

    2

--- backend/src/tests/background_tasks/translate_file_created_by_public_request/__init__.py
+++ backend/src/tests/background_tasks/translate_file_created_by_public_request/__init__.py
@@ -2,15 +2,15 @@
 
 def add_fresh_jobs(background_task_scheduler, BACKGROUND_TASKS):
     
-    from tests.background_tasks.delete_invalid_file.main import test_get_task_id_from_task_result_file_path
+    from tests.background_tasks.translate_file_created_by_public_request.main import test_all
     
-    background_task_1_conf = BACKGROUND_TASKS['test_delete_invalid_file']
+    background_task_1_conf = BACKGROUND_TASKS['test_translate_file_created_by_public_request']
     
     if background_task_scheduler.get_job(background_task_1_conf.ID) != None:
         background_task_scheduler.remove_job(background_task_1_conf.ID)
  
     background_task_scheduler.add_job(
-        test_get_task_id_from_task_result_file_path,
+        test_all,
         id=background_task_1_conf.ID,
         trigger=background_task_1_conf.TRIGGER,
         **background_task_1_conf.CONFIG
